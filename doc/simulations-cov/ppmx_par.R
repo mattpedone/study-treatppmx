2c623f6d3c83569e4d85bfa0c5ad315a4a16ffdc
mattpedone <mattpedone7@gmail.com>
2021-09-06 17:59:56 +0200

    parallelized prediction

---
+++ doc/simulations-cov/ppmx_par.R
@@ -0,0 +1,60 @@
+rm(list=ls())
+load("data/SimuOutsce2.rda")
+library(treatppmx)
+library(parallel)
+library(doParallel)
+source("src/countUT.R");  
+
+K <- 2 #repliche
+npat <- 152
+predAPT_all<-array(0,dim=c(npat,9,K))
+
+wk <- c(0, 40, 100)
+
+for(k in 1:K){
+  #predAPT<-matrix(1,nrow= npat,ncol=10);  ### ut1,ut2,trt,cluster
+  cor_all <- parallel::detectCores()-1#cores to be allocated
+  registerDoParallel(cores = cor_all)
+  
+  X <- data.frame(t(mydata))[, -c(51:92)]#data.frame(mydata)#
+  Z <- data.frame(cbind(myx2, myx3))#data.frame(orgx)#
+  Y <- mytot[,,k]
+  
+  modelpriors <- list()
+  modelpriors$hP0_m0 <- rep(0, ncol(Y)); modelpriors$hP0_L0 <- diag(10, ncol(Y))
+  modelpriors$hP0_nu0 <- ncol(Y) + 2; modelpriors$hP0_V0 <- diag(10, ncol(Y))
+  
+  n_aux <- 5 # auxiliary variable for Neal's Algorithm 8
+  vec_par <- c(0.0, 1.0, .5, 1.0, 2.0, 2.0, 0.1)
+  #double m0=0.0, s20=10.0, v=.5, k0=1.0, nu0=2.0, n0 = 2.0;
+  iterations <- 25#000; 
+  burnin <- 5#000; 
+  thinning <- 1
+  
+  nout <- (iterations-burnin)/thinning
+  predAPT <- c()
+  
+  myres <- foreach(sub = 1:npat, .combine = rbind) %dopar%
+    {
+    out_ppmx <- my_dm_ppmx_ct(y = data.matrix(Y[-sub,]), X = data.frame(X[-sub,]), Xpred = data.frame(X[sub,]),
+                                z = data.frame(Z[-sub,]), zpred = data.frame(Z[sub,]), asstreat = trtsgn[-sub], #treatment,
+                                alpha = 1, CC = n_aux, reuse = 1,
+                                PPMx = 1, similarity = 2, consim = 2,  gowtot = 1,
+                                alphagow = 5, calibration = 2, coardegree = 2,
+                                similparam = vec_par, modelpriors, update_hierarchy = T,
+                                iter = iterations, burn = burnin, thin = thinning, hsp = T)
+    #posterior predictive probabilities ----
+    A0 <- c(apply(out_ppmx$ypred, c(1,2,3), mean));#A0
+    return(A0)
+    }
+  
+  ##treatment prediction with utility function ----
+  A1 <- myres[,1:3]%*%wk; A2 <- myres[,4:6]%*%wk
+  predAPT_all[,1,k]<-A1
+  predAPT_all[,2,k]<-A2
+  myt <- as.numeric(A1<A2) +1
+  predAPT_all[,3,k]<-myt
+  predAPT_all[,4:9,k]<-myres
+}
+
+#save(predAPT_all, file="output/scen50/rep1.rda")
--- output/scen50/instructions.md
+++ output/scen50/instructions.md
@@ -3,3 +3,4 @@
 These are the results for the simulations study with Coarsened DD, exploiting 50 predictive biomarkers.
 Results are generated using the file `PPMX.R` in the `/doc/simulations-cov` folder. 
 To process them use the `/doc/simulations-cov/gatherppmx.R` file. 
+For a more efficient script use the file `ppmx_par.R` to perform parallelized prediction.
