c116fb8514987583463b5712c1255b1d55072426
mattpedone <mattpedone7@gmail.com>
2021-10-15 10:50:32 +0200

    script for generate scenarios

---
+++ R/genscen.R
@@ -0,0 +1,10 @@
+rm(list=ls())
+library(treatppmx)
+
+### ----
+# This script is used to generate all the scenarios
+###
+
+# this is what I called scenario 2, but with 20 covariates instead of 10
+treatppmx::genmech(npred = 20, progscen = 1, predscen = 1, nset = 30, save = T, 
+                   filename = "scenarios/modscenario2")
