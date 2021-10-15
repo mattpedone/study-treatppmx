c111e3de0292580114a6f5a25dadc8ce04e7ed6c
mattpedone <mattpedone7@gmail.com>
2021-10-15 17:01:41 +0200

    parallelized script for simulations

--- R/parallel_loocv_ppmx.R
+++ R/parallel_loocv_ppmx.R
@@ -3,10 +3,15 @@ load("data/modscenario2.rda")
 library(treatppmx)
 library(parallel)
 library(doParallel)
+library(mcclust)
+library(mcclust.ext)
 
-K <- 3 #repliche
+K <- 10 #repliche
 npat <- length(trtsgn)
+
 predAPT_all <- array(0, dim = c(npat, 9, K))
+nclust_all <- matrix(0, nrow = K, ncol = 6)
+gof_all <- matrix(0, nrow = K, ncol = 2)
 
 wk <- c(0, 40, 100)
 
@@ -17,7 +22,7 @@ for(k in 1:K){
   
   #X <- data.frame(t(mydata))[, -c(11:92)]#data.frame(mydata)#
   X <- data.frame(mydata)
-  Z <- data.frame(cbind(myx2, myx3))#data.frame(orgx)#
+  Z <- data.frame(cbind(myz2, myz3))#data.frame(orgx)#
   Y <- mytot[,,k]
   
   modelpriors <- list()
@@ -27,35 +32,95 @@ for(k in 1:K){
   n_aux <- 5 # auxiliary variable for Neal's Algorithm 8
   vec_par <- c(0.0, 1.0, .5, 1.0, 2.0, 2.0, 0.1)
   #double m0=0.0, s20=10.0, v=.5, k0=1.0, nu0=2.0, n0 = 2.0;
-  iterations <- 10#0000; 
-  burnin <- 1#5000; 
-  thinning <- 1#0
+  iterations <- 100000; 
+  burnin <- 25000; 
+  thinning <- 10
   
   nout <- (iterations-burnin)/thinning
   predAPT <- c()
   
   myres <- foreach(sub = 1:npat, .combine = rbind) %dopar%
     {
-    out_ppmx <- my_dm_ppmx_ct(y = data.matrix(Y[-sub,]), X = data.frame(X[-sub,]), Xpred = data.frame(X[sub,]),
-                                z = data.frame(Z[-sub,]), zpred = data.frame(Z[sub,]), asstreat = trtsgn[-sub], #treatment,
-                                alpha = 1, CC = n_aux, reuse = 1,
-                                PPMx = 1, similarity = 2, consim = 2,  #gowtot = 1,
-                                #alphagow = 5, 
-                              calibration = 2, coardegree = 1,
-                                similparam = vec_par, modelpriors, update_hierarchy = T,
-                                iter = iterations, burn = burnin, thin = thinning, hsp = T)
+    out_ppmx <- tryCatch(expr = my_dm_ppmx_ct(y = data.matrix(Y[-sub,]), X = data.frame(X[-sub,]), 
+                              Xpred = data.frame(X[sub,]), Z = data.frame(Z[-sub,]), 
+                              Zpred = data.frame(Z[sub,]), asstreat = trtsgn[-sub], #treatment,
+                              PPMx = 1, cohesion = 2, alpha = 1, sigma = 0.2,
+                              similarity = 2, consim = 2, similparam = vec_par, 
+                              calibration = 2, coardegree = 1, modelpriors, update_hierarchy = T,
+                              hsp = T, iter = iterations, burn = burnin, thin = thinning, 
+                              mhtunepar = c(0.05, 0.05), CC = n_aux, reuse = 1, nclu_init = 5), error = function(e){FALSE})
+    
+    #number of a cluster, mean, binder &varinf ----
+    mc <- apply(out_ppmx$nclu, 1, mean)
+    trt <- trtsgn[-sub]
+    num_treat <- table(trt)
+    
+    cls1 <- t(as.matrix(out_ppmx$label[[1]]))[,c(1:num_treat[1])]
+    psm1 <- comp.psm(cls1)
+    mc_b1 <- minbinder.ext(psm1); 
+    mc_vi1 <- minVI(psm1); 
+    
+    cls2 <- t(as.matrix(out_ppmx$label[[2]]))[,c(1:num_treat[2])]
+    psm2 <- comp.psm(cls2)
+    mc_b2 <- minbinder.ext(psm2); 
+    mc_vi2 <- minVI(psm2); 
+    
+    mc_b <- c(max(mc_b1$cl), max(mc_b2$cl))
+    mc_vi <- c(max(mc_vi1$cl), max(mc_vi2$cl))
+    
     #posterior predictive probabilities ----
-    A0 <- c(apply(out_ppmx$ypred, c(1,2,3), mean));#A0
-    return(A0)
+    A0 <- c(apply(out_ppmx$ypred, c(1,2,3), mean), mc, mc_b, mc_vi, out_ppmx$WAIC, out_ppmx$lpml);#A0
+    ifelse(is.logical(out_ppmx), return(rep(0, 14)), return(A0))
     }
   
   ##treatment prediction with utility function ----
-  A1 <- myres[,1:3]%*%wk; A2 <- myres[,4:6]%*%wk
-  predAPT_all[,1,k]<-A1
-  predAPT_all[,2,k]<-A2
-  myt <- as.numeric(A1<A2) +1
-  predAPT_all[,3,k]<-myt
-  predAPT_all[,4:9,k]<-myres
+  cat("errori: ", which(rowSums(myres) == 0), "\n")
+  sellines <- which(rowSums(myres) != 0)
+  A1 <- myres[sellines, 1:3]%*%wk 
+  A2 <- myres[sellines, 4:6]%*%wk
+  predAPT_all[, 1, k] <- A1
+  predAPT_all[, 2, k] <- A2
+  myt <- as.numeric(A1 < A2) + 1
+  predAPT_all[, 3, k] <- myt
+  predAPT_all[, 4:9, k] <- myres[sellines, 1:6]
+  
+  nclust_all[k,] <- apply(myres[sellines, 7:12], 2, mean)
+  gof_all[k,] <- apply(myres[sellines, 13:14], 2, mean)
 }
 
+mywk1 <- myprob[[1]]%*%wk
+mywk2 <- myprob[[2]]%*%wk
+optrt <- as.numeric(mywk2 > mywk1) + 1
+utsum <- sum(abs(mywk2 - mywk1)) 
+utdiff <- abs(as.numeric(mywk2 - mywk1))
+
+#MOT
+PPMXCT <-  apply(abs(predAPT_all[, 3, 1:K] - optrt), 2, sum)
+MOT <- c(round(mean(PPMXCT), 4), round(sd(PPMXCT), 4))
+
+#MTUg
+PPMXpp <-  -(2*apply(abs((predAPT_all[, 3, 1:K] - optrt)) * utdiff, 2, sum) - utsum);
+MTUg <- c(round(mean(PPMXpp/utsum), 4), round(sd(PPMXpp/utsum), 4))
+
+#NPC
+PPMXCUT<-as.vector(npc(predAPT_all[, 4:9,], trtsgn, myoutot));
+NPC <- c(round(mean(PPMXCUT), 4), round(sd(PPMXCUT), 4))
+
+#NCLU
+NC <- c(apply(nclust_all[,1:2], 2, mean), apply(nclust_all[,1:2], 2, sd))
+BI <- c(apply(nclust_all[,3:4], 2, mean), apply(nclust_all[,3:4], 2, sd))
+VI <- c(apply(nclust_all[,5:6], 2, mean), apply(nclust_all[,5:6], 2, sd))
+
+#FIT
+WAIC <- c(mean(gof_all[,1]), sd(gof_all[,1]))
+lpml <- c(mean(gof_all[,2]), sd(gof_all[,2]))
+
+#results
+resPPMX <- rbind(MOT, MTUg, NPC, WAIC, lpml)
+colnames(resPPMX) <- c("mean", "sd")
+resPPMX
+
+numofclu <- rbind(NC, BI, VI)
+colnames(numofclu) <- c("mean trt 1", "mean trt 2", "sd trt 1", "sd trt 2")
+numofclu
 #save(predAPT_all, file="output/scen50/rep1.rda")
