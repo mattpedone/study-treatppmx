d3120a8f5cc0320b142f0b5dbd6da3bfae91f670
mattpedone <mattpedone7@gmail.com>
2021-11-04 18:17:19 +0100

    script for ppmx prior sim study

---
+++ R/prior_parallel.R
@@ -0,0 +1,97 @@
+library(parallel)
+library(doParallel)
+library(dplyr)
+library(tibble)
+library(tidyselect)
+library(ggplot2)
+library(treatppmx)
+set.seed(121)
+
+K <- 2
+X <- data.frame(simupats)
+X <- X[,c(1:10)]
+nobs <- nrow(X)
+
+vec_par <- c(0.0, 10.0, .5, 1.0, 2.0, 2.0, 0.1)
+#double m0=0.0, s20=10.0, v=.5, k0=1.0, nu0=2.0, n0 = 2.0;
+iterations <- 100#00#500
+burnin <- 25#00#00
+thinning <- 1#0
+
+nout <- (iterations-burnin)/thinning
+
+cor_all <- parallel::detectCores()-1#cores to be allocated
+registerDoParallel(cores = cor_all)
+
+res_121 <- foreach(sub = 1:K, .combine = rbind) %dopar%
+  {
+    out_ppmx_prior <- prior_ppmx(X = X, PPMx = 1, cohesion = 1, alpha = 2,
+                                 sigma = .1, similarity = 1, consim = 1,
+                                 similparam = vec_par, calibration = 0,
+                                 coardegree = 1, iter = iterations, burn = burnin,
+                                 thin = thinning, nclu_init = 1)
+  }
+
+res_122 <- foreach(sub = 1:K, .combine = rbind) %dopar%
+  {
+    out_ppmx_prior <- prior_ppmx(X = X, PPMx = 1, cohesion = 1, alpha = 2,
+                                 sigma = .1, similarity = 1, consim = 2,
+                                 similparam = vec_par, calibration = 0,
+                                 coardegree = 2, iter = iterations, burn = burnin,
+                                 thin = thinning, nclu_init = 1)
+  }
+
+res_221 <- foreach(sub = 1:K, .combine = rbind) %dopar%
+  {
+    out_ppmx_prior <- prior_ppmx(X = X, PPMx = 1, cohesion = 1, alpha = 2,
+                                 sigma = .1, similarity = 1, consim = 1,
+                                 similparam = vec_par, calibration = 2,
+                                 coardegree = 2, iter = iterations, burn = burnin,
+                                 thin = thinning, nclu_init = 1)
+  }
+
+res_222 <- foreach(sub = 1:K, .combine = rbind) %dopar%
+  {
+    out_ppmx_prior <- prior_ppmx(X = X, PPMx = 1, cohesion = 1, alpha = 2,
+                                 sigma = .1, similarity = 1, consim = 2,
+                                 similparam = vec_par, calibration = 0,
+                                 coardegree = 2, iter = iterations, burn = burnin,
+                                 thin = thinning, nclu_init = 1)
+  }
+
+res <- t(rbind(apply(res_121/nout, 2, mean), 
+               apply(res_122/nout, 2, mean), apply(res_21/nout, 2, mean), 
+               apply(res_221/nout, 2, mean), apply(res_221/nout, 2, mean), 
+               apply(res_121/nout, 2, sd), 
+               apply(res_122/nout, 2, sd), apply(res_21/nout, 2, sd), 
+               apply(res_221/nout, 2, sd), apply(res_221/nout, 2, sd)))
+
+res <- (res[which(rowSums(res) != 0),])
+res <- rownames_to_column(as.data.frame(res), var = "cluster")
+newres <- cbind(similarity = c(rep("NNcoa1", nrow(res)), rep("NNcoa2", nrow(res)), 
+                               rep("NNIGcoa1", nrow(res)), rep("NNIGcoa2", nrow(res))),
+                cluster = rep(res[,1], 4),
+                freq = c(unlist(res[, c(2:5)])),
+                sd = c(unlist(res[, c(6:11)])))
+
+dfres <- newres %>%
+  as_tibble() %>% # lo trasformo in un dataframe
+  #tibble::rownames_to_column(var = "cluster") %>%
+  mutate(cluster = as.numeric(cluster)) %>%
+  mutate(freq = as.numeric(freq)) %>%
+  mutate(sd = as.numeric(sd))
+
+dfres
+  #select_if(rowSums(res) != 0) %>% #tolgo i casi = 00
+  #mutate(Similarity = c("ppmx", "ppm"), .before = 1)
+
+#dfres <- tidyr::gather(dfres, cluster)
+
+
+ggplot(dfres, aes(x=cluster, y=freq, group=similarity, color=similarity)) +
+  geom_errorbar(aes(ymin=freq-sd, ymax=freq+sd), width=.1) +
+  geom_line() +
+  geom_point()+
+  scale_color_brewer(palette="Paired")+
+  theme_minimal()
+
