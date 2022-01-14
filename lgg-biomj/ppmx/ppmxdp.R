e60c70f4fa9086474f3750cee2a365b207464b8a
mattpedone <mattpedone7@gmail.com>
2022-01-14 18:06:46 +0100

    parametri

--- lgg-biomj/ppmx/ppmxdp.R
+++ lgg-biomj/ppmx/ppmxdp.R
@@ -32,13 +32,13 @@ Y
 
 modelpriors <- list()
 modelpriors$hP0_m0 <- rep(0, ncol(Y)); modelpriors$hP0_L0 <- diag(10, ncol(Y))
-modelpriors$hP0_nu0 <- ncol(Y) + 2; modelpriors$hP0_V0 <- diag(.1, ncol(Y))
+modelpriors$hP0_nu0 <- ncol(Y) + 2; modelpriors$hP0_V0 <- diag(1.0, ncol(Y))
 
 #n_aux <- 5 # auxiliary variable for Neal's Algorithm 8
 vec_par <- c(0.0, 1.0, .5, 1.0, 2.0, 2.0, 0.1)
 #double m0=0.0, s20=10.0, v=.5, k0=1.0, nu0=2.0, n0 = 2.0;
-iterations <- 50000 
-burnin <- 20000
+iterations <- 5000 
+burnin <- 2000
 thinning <- 10
 
 nout <- (iterations-burnin)/thinning
@@ -49,7 +49,7 @@ myres <- foreach(sub = 1:npat, .combine = rbind) %dopar%
     out_ppmx <- tryCatch(expr = ppmxct(y = data.matrix(Y[-sub,]), X = data.frame(X[-sub,]), 
                                        Xpred = data.frame(X[sub,]), Z = data.frame(Z[-sub,]), 
                                        Zpred = data.frame(Z[sub,]), asstreat = trtsgn[-sub], #treatment,
-                                       PPMx = 1, cohesion = 2, alpha = 10, sigma = 0.25,
+                                       PPMx = 1, cohesion = 2, alpha = 10, sigma = 0.01,
                                        similarity = 2, consim = 2, similparam = vec_par, 
                                        calibration = 2, coardegree = 2, modelpriors, 
                                        update_hierarchy = T,
